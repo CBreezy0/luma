47cdc01b0e76f5db86635d822bbf859367505659
Chris Bryant <breezy@Chriss-MacBook-Air.local>
2026-03-08 15:32:33 -0400

    ci: fix Xcode Cloud by generating Flutter iOS config files before build

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 set -e
 
+echo "Installing Flutter dependencies"
+
+flutter pub get
+
+echo "Generating Flutter iOS build files"
+
+flutter build ios --debug --no-codesign
+
+echo "Installing CocoaPods"
+
 cd ios
 pod install
+cd ..
+
+echo "Post-clone setup complete"
